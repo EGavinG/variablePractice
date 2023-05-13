234315656637f15ff6acd0164b63002d3372977e
Gavin Garcia <edwardgavingarcia@yahoo.com>
2023-05-12 22:15:58 -0500

    Fixed Format

--- variable.js
+++ variable.js
@@ -1,11 +1,14 @@
-var startingAge = "30"
-var luckyNumber = "4"
-var birthYear = "1993"
+// number variables 
+var startingAge = 30
+var luckyNumber = 4
+var birthYear = 1993
 
+// string variables
 var email = "edwardgavingarcia@yahoo.com"
 var mothersName = "Johanna"
 var petsName = "Renton"
 
-var lovesTuring = "true"
-var upLate = "true"
-var namedFileWrong = "true"
+// Boolean variables
+var lovesTuring = true
+var upLate = true
+var namedFileWrong = true
